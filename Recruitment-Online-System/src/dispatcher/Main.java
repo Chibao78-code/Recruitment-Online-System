4b08338e12e83b16a73d5540a32fd7fca426f268
Chibao78-code <ddinhchibao@gmail.com>
2025-08-28 07:34:49 +0700

    update

--- Recruitment-Online-System/src/dispatcher/Main.java
+++ Recruitment-Online-System/src/dispatcher/Main.java
@@ -13,104 +13,132 @@ import java.util.Scanner;
 import java.util.Set;
 import models.KolRegister;
 import models.KolPlatForm;
+import tools.Acceptable;
 import tools.Inputter;
 /**
  *
  * @author zzzdi
  */
 public class Main {
-        private static final int IMPORT_USER = 1;
-    private static final int UPDATE_USER = 2;
-    private static final int DISPLAY_USER = 3;
-    private static final int UPDATE_GUEST = 4;
-    private static final int DISPLAY_GUEST = 5;
-    private static final int DELETE_GUEST = 6;
-    private static final int DISPLAY_VACANT_ROOM = 7;
-    private static final int MONTHLY_REVENUE_REPORT = 8;
+         private static final int REGISTER_KOL = 1;
+    private static final int UPDATE_REGISTER = 2;
+    private static final int DISPLAY_REGISTERS = 3;
+    private static final int DELETE_REGISTER = 4;
+    private static final int SEARCH_BY_NAME = 5;
+    private static final int FILTER_BY_CATEGORY = 6;
+    private static final int DISPLAY_REGISTER_NUMBERS_BY_FLATFORM = 7;
+    private static final int SAVE_REGISTER = 8;
     private static final int EXIT = 9;
-    private static final int RETURN_MAIN__MENU = 2;
-    private static final String KOL_FILE = "KOLList.csv";
-    private static final String USER_FILE = "kol_registrations.dat";
+    private static final int DISPLAY_KOLS = 11;
+    private static final int RETURN_MAIN_MENU = 2;
+    private static final String PLATFORM_FILE = "KOLList.csv";
+    private static final String REGISTER_FILE = "kol_registrations.dat";
+    private static boolean isSaved = false;
 
     private static Inputter inputter;
-    private static KolRegisters users;
-    private static KolPlatForms kols;
+    private static KolRegisters registers;
+    private static KolPlatForms platforms;
     private static Scanner scanner;
 
     private static void initializeSystem() {
         inputter = new Inputter();
-        kols = new KolPlatForms(KOL_FILE);
-        users = new KolRegisters(USER_FILE);
+        platforms = new KolPlatForms(PLATFORM_FILE);
+        registers = new KolRegisters(REGISTER_FILE);
         scanner = new Scanner(System.in);
     }
 
     private static void displayMainMenu() {
         System.out.println("\n----------MAIN MENU------------");
-        System.out.println("1. Register user");
-        System.out.println("2. Update user");
-        System.out.println("3. Enter Guest Information");
-        System.out.println("4. Update Guest Stay Information");
-        System.out.println("5. Search Guest by National ID");
-        System.out.println("6. Delete Guest Reservation Before Arrival");
-        System.out.println("7. List Vacant Rooms");
-        System.out.println("8. Monthly Revenue Report");
+        System.out.println("1. New Registration");
+        System.out.println("2. Update Registration Information");
+        System.out.println("3. Display Registered List");
+        System.out.println("4. Delete Registration Information");
+        System.out.println("5. Search KOLs by Name");
+        System.out.println("6. Filter Data by Category");
+        System.out.println("7. Statistics of Registration Numbers by Platform");
+        System.out.println("8. Save Data to File");
         System.out.println("9. Exit");
+        System.out.println("11. DisplayKols");
         System.out.print("Enter Test Case No. : ");
     }
-        private static void runMainMenu() {
-        int testcase;
-        do {
+        private static int getChoice() {
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    private static void runMainMenu() {
+        while (true) {
             displayMainMenu();
-            testcase = getChoice();
-            processMenuChoice(testcase);
-        } while (testcase != EXIT);
+            int choice = getChoice();
+            processMenuChoice(choice);
+        }
     }
 
     private static void processMenuChoice(int testCase) {
         switch (testCase) {
-            case IMPORT_USER:
-                handleRegisterUser();
+            case REGISTER_KOL:
+                handleRegisterKol();
                 break;
-            case UPDATE_USER:
-                handleUpdateUser();
+            case UPDATE_REGISTER:
+                handleUpdateRegister();
                 break;
-            case DISPLAY_USER:
-                handleDisplayUser();
+            case DISPLAY_REGISTERS:
+                handleDisplayRegister();
+                break;
+            case DELETE_REGISTER:
+                handleDeleteRegister();
+                break;
+            case SEARCH_BY_NAME:
+                handleSearchByName();
+                break;
+            case FILTER_BY_CATEGORY:
+                handleFilterByCategory();
+                break;
+            case DISPLAY_REGISTER_NUMBERS_BY_FLATFORM:
+                handleDisplayRegisterNumbersByPlatform();
+                break;
+            case SAVE_REGISTER:
+                handleSaveRegister();
+                isSaved = true;
                 break;
             case EXIT:
-                //handleExit();
+                handleExit(isSaved);
+                break;
+            case DISPLAY_KOLS:
+                handleDisplayKols();
                 break;
             default:
-                throw new AssertionError();
+                System.out.println("Invalid choice. Please enter a number from 1 to 9.");
+                break;
         }
     }
-        private static void handleRegisterUser() {
+
+    private static void handleRegisterKol() {
         int option;
         do {
-            users.addNew(inputter.inputUser(false, users, kols));
+            registers.addNew(inputter.inputRegister(false, registers, platforms));
             System.out.println("1. Continue add guest");
             System.out.println("2. Return to the main menu");
             System.out.println("Enter your option: ");
             option = Integer.parseInt(scanner.nextLine());
-        } while (option != RETURN_MAIN__MENU);
+        } while (option != RETURN_MAIN_MENU);
     }
 
-    private static void handleUpdateUser() {
+    private static void handleUpdateRegister() {
         int option;
         do {
-            users.update(inputter.inputUser(true, users, kols));
+            registers.update(inputter.inputRegister(true, registers, platforms));
             System.out.println("1. Continue update guest");
             System.out.println("2. Return to the main menu");
             System.out.println("Enter your option: ");
             option = Integer.parseInt(scanner.nextLine());
-        } while (option != RETURN_MAIN__MENU);
+        } while (option != RETURN_MAIN_MENU);
     }
 
-    private static void handleDisplayUser() {
-        if(users==null||users.isEmpty()){
+    private static void handleDisplayRegister() {
+        if (registers == null || registers.isEmpty()) {
             System.err.println("No Kols have registered yet");
-        }else{
-            users.showAll(kols);
+        } else {
+            registers.showAll(null, platforms);
         }
     }
 
@@ -118,5 +146,45 @@ public class Main {
         initializeSystem();
         runMainMenu();
     }
+    private static void handleDeleteRegister() {
+        String keyword = "";
+        while (true) {
+            System.out.println("Input kol code for delete(kol code must start with(BT,FS,BC,GM,TL) and 6 numberics after): ");
+            keyword = scanner.nextLine();
+            if (keyword.matches(Acceptable.idRegex)) {
+                break;
+            } else {
+                System.err.println("kol code must start with(BT,FS,BC,GM,TL) and 6 numberics after");
+            }
+        }
+        KolRegister user = registers.searchById(keyword);
+        if (user == null) {
+            System.err.println("This KOL has not registered yet");
+        } else {
+            user.display(platforms);
+            System.out.println("Are you sure you want to delete this registration? (Y/N): ");
+            String choice = scanner.nextLine().trim();
+            if (choice.equalsIgnoreCase("Y")) {
+                registers.delete(user);
+                System.out.println("The registration has been successfully deleted.");
+            } else if (choice.equalsIgnoreCase("N")) {
+                System.out.println("The registration has NOT been deleted.");
+            } else {
+                System.out.println("Invalid choice. No action taken.");
+            }
+        }
+    }
+
+    private static void handleSearchByName() {
+        System.out.println("Input name for search: ");
+        String keyword = scanner.nextLine();
+        List<KolRegister> list = registers.filterByName(keyword);
+        if (list == null || list.isEmpty()) {
+            System.err.println("No one matches the search criteria!");
+        } else {
+            registers.showAll(list, platforms);
+        }
+    }
+
 
 }
